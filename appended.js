976ad16d00ce5c7d2e2e1b34bdc77506b4eb0c94
asadali16092001 <aj7027254@gmail.com>
2024-09-15 03:21:17 -0700

    changing css

--- appended.js
+++ appended.js
@@ -4,3 +4,11 @@ document.querySelector('.mobile-nav-toggle').addEventListener('click', function(
     navMenu.classList.toggle('active'); // Add or remove the 'active' class
   });
   
+  document.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+       }
+    });
